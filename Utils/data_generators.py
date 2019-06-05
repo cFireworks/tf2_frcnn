e12657d94cf153d9ae91501ebfc8634a29930aef
cFireworks <xwtfd@outlook.com>
2019-06-05 21:35:17 +0800

    complete data_generators and data_augment

--- Utils/data_generators.py
+++ Utils/data_generators.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import random
 import copy
-# from . import data_augment
+from Utils import data_augment
 import threading
 import itertools
 
@@ -120,9 +120,9 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height, feature_
     num_anchors_for_bbox = np.zeros(num_bboxes).astype(int)
     best_anchor_for_bbox = -1 * np.ones((num_bboxes, 4)).astype(int)
     best_iou_for_bbox = np.zeros(num_bboxes).astype(np.float32)
-    # ????
-    best_x_for_bbox = np.zeros((num_bboxes, 4)).astype(int)
-    best_dx_for_bbox = np.zeros((num_bboxes, 4)).astype(np.float32)
+    # gt-box 对应具有最好iou的anchor的坐标  以及  对应的线性变换结果 t
+    best_xy_for_bbox = np.zeros((num_bboxes, 4)).astype(int)
+    best_t_for_bbox = np.zeros((num_bboxes, 4)).astype(np.float32)
 
     # get the GT box coordinates, and resize to account for image resizing
     # convert the label bbox to resized image, just change the ratio according to resize/original_size
@@ -171,7 +171,8 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height, feature_
                     for bbox_num in range(num_bboxes):
                         # get IOU of the current GT box and the current anchor box,
                         # these coords are based on resized img input
-                        curr_iou = iou([gta[bbox_num, 0], gta[bbox_num, 2], gta[bbox_num, 1], gta[bbox_num, 3]], [x1_anc, y1_anc, x2_anc, y2_anc])
+                        curr_iou = iou([gta[bbox_num, 0], gta[bbox_num, 2], gta[bbox_num, 1], gta[bbox_num, 3]],
+                                       [x1_anc, y1_anc, x2_anc, y2_anc])
 
                         # calculate the regression targets if they will be needed
                         if curr_iou > best_iou_for_bbox[bbox_num] or curr_iou > C.rpn_max_overlap:
@@ -185,48 +186,192 @@ def calc_rpn(C, img_data, width, height, resized_width, resized_height, feature_
                             tw = np.log((gta[bbox_num, 1] - gta[bbox_num, 0]) / (x2_anc - x1_anc))
                             th = np.log((gta[bbox_num, 3] - gta[bbox_num, 2]) / (y2_anc - y1_anc))
 
+                        if img_data['bboxes'][bbox_num]['class'] != 'bg':
+
+                            # all GT boxes should be mapped to an anchor box, so we keep track of which anchor box was best
+                            if curr_iou > best_iou_for_bbox[bbox_num]:
+                                best_anchor_for_bbox[bbox_num] = [jy, ix, anchor_ratio_idx, anchor_size_idx]
+                                best_iou_for_bbox[bbox_num] = curr_iou
+                                best_xy_for_bbox[bbox_num] = [x1_anc, x2_anc, y1_anc, y2_anc]
+                                best_t_for_bbox[bbox_num] = [tx, ty, tw, th]
+
+                            if curr_iou >= C.rpn_max_overlap:
+                                bbox_type = 'pos'
+                                num_anchors_for_bbox[bbox_num] += 1
+                                # we update the regression layer target if this IOU is the best for the current (x,y) and anchor position
+                                if curr_iou > best_iou_for_loc:
+                                    best_iou_for_loc = curr_iou
+                                    best_regr = (tx, ty, tw, th)
+
+                            # if the IOU is >0.3 and <0.7, it is ambiguous and no included in the objective
+                            if C.rpn_min_overlap < curr_iou < C.rpn_max_overlap:
+                                # gray zone between neg and pos
+                                if bbox_type != 'pos':
+                                    bbox_type = 'neutral'
+
+                    # turn on or off outputs depending on IOUs
+                    # box type has neg,pos, neutral
+                    # neg: the box is backgronud
+                    # pos: the box has RPN
+                    # neutral: normal box with a RPN
+                    # 设置三个量： y_is_box_valid  y_rpn_overlap  y_rpn_regr
+                    if bbox_type == 'neg':
+                        y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+                        y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
+                    elif bbox_type == 'neutral':
+                        y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
+                        y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 0
+                    elif bbox_type == 'pos':
+                        y_is_box_valid[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+                        y_rpn_overlap[jy, ix, anchor_ratio_idx + n_anchratios * anchor_size_idx] = 1
+                        start = 4 * (anchor_ratio_idx + n_anchratios * anchor_size_idx)
+                        y_rpn_regr[jy, ix, start:start + 4] = best_regr
+
+    # we ensure that every bbox has at least one positive RPN region
+    for idx in range(num_anchors_for_bbox.shape[0]):
+        if num_anchors_for_bbox[idx] == 0:
+            # no box with an IOU greater than zero ...
+            if best_anchor_for_bbox[idx] == -1:
+                continue
+            y_is_box_valid[
+                best_anchor_for_bbox[idx, 0], best_anchor_for_bbox[idx, 1], best_anchor_for_bbox[
+                    idx, 2] + n_anchratios * best_anchor_for_bbox[idx, 3]
+            ] = 1
+            y_rpn_overlap[
+                best_anchor_for_bbox[idx, 0], best_anchor_for_bbox[idx, 1], best_anchor_for_bbox[
+                    idx, 2] + n_anchratios * best_anchor_for_bbox[idx, 3]
+            ] = 1
+            start = 4 * (best_anchor_for_bbox[idx, 2] + n_anchratios * best_anchor_for_bbox[idx, 3])
+            y_rpn_regr[best_anchor_for_bbox[idx, 0], best_anchor_for_bbox[idx, 1], start:start + 4] = best_t_for_bbox[
+                                                                                                      idx, :]
+    y_rpn_overlap = np.transpose(y_rpn_overlap, (2, 0, 1))
+    y_rpn_overlap = np.expand_dims(y_rpn_overlap, axis=0)
+
+    y_is_box_valid = np.transpose(y_is_box_valid, (2, 0, 1))
+    y_is_box_valid = np.expand_dims(y_is_box_valid, axis=0)
+
+    y_rpn_regr = np.transpose(y_rpn_regr, (2, 0, 1))
+    y_rpn_regr = np.expand_dims(y_rpn_regr, axis=0)
+
+    # pos_locs is a tuple of selected coordinate ([], [], [])
+    pos_locs = np.where(np.logical_and(y_rpn_overlap[0, :, :, :] == 1, y_is_box_valid[0, :, :, :] == 1))
+    neg_locs = np.where(np.logical_and(y_rpn_overlap[0, :, :, :] == 0, y_is_box_valid[0, :, :, :] == 1))
+
+    # number of pos anchors
+    num_pos = len(pos_locs[0])
+    num_neg = len(neg_locs[0])
+
+    # 限制负样本anchor的数量， 将 y_is_box_valid 中部分代表 neg box 的元素修改为 0
+    # one issue is that the RPN has many more negative than positive regions, so we turn off some of the negative
+    # regions. We also limit it to 256 regions.
+    num_regions = 256
+
+    # 限制 pos 类的 box 个数不超过 num_regions 的一半
+    if len(num_pos > num_regions / 2):
+        val_locs = random.sample(range(num_pos), num_pos - num_regions / 2)
+        y_is_box_valid[0, pos_locs[0][val_locs], pos_locs[1][val_locs], pos_locs[2][val_locs]] = 0
+        num_pos = num_regions / 2
+
+    # 限制 neg 类的 box 个数不超过 num_regions 的一半
+    if num_neg + num_pos > num_regions:
+        val_locs = random.sample(range(len(neg_locs[0])), len(neg_locs[0]) - num_pos)
+        y_is_box_valid[0, neg_locs[0][val_locs], neg_locs[1][val_locs], neg_locs[2][val_locs]] = 0
+
+    y_rpn_cls = np.concatenate([y_is_box_valid, y_rpn_overlap], axis=1)
+    y_rpn_regr = np.concatenate([np.repeat(y_rpn_overlap, 4, axis=1), y_rpn_regr], axis=1)
+
+    return np.copy(y_rpn_cls), np.copy(y_rpn_regr)
 
 
+class threadsafe_iter:
+    """Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
+
+    def __iter__(self):
+        return self
+
+    # change function name 'next' to '__next__'
+    def __next__(self):
+        with self.lock:
+            return self.it.__next__()
 
 
+def threadsafe_generator(f):
+    """A decorator that takes a generator function and makes it thread-safe.
+    """
+
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+
+    return g
 
 
+def get_anchor_gt(all_img_data, class_count, C, img_length_calc_function, mode='train'):
+    # The following line is not useful with Python 3.5, it is kept for the legacy
+    # all_img_data = sorted(all_img_data)
+    # 返回一个迭代器，因为使用了死循环，所以可以不断地从迭代器提取图像数据
+    # 通过 shuffle 打乱图像顺序， 通过图像增强使得图像在 翻转 和 旋转 上具有随机性
+    # 达到扩充样本功能
 
+    sample_selector = SampleSelector(class_count)
 
+    while True:
+        if mode == 'train':
+            random.shuffle(all_img_data)
 
+        for img_data in all_img_data:
+            try:
+                if C.balanced_classes and sample_selector.skip_sample_for_balanced_class(img_data):
+                    continue
 
+                # read in image, and optionally add augmentation
+                if mode == 'train':
+                    img_data_aug, x_img = data_augment.augment(img_data, C, augment=True)
+                else:
+                    img_data_aug, x_img = data_augment.augment(img_data, C, augment=False)
 
+                (width, height) = (img_data_aug['width'], img_data_aug['height'])
+                (rows, cols, _) = x_img.shape
 
+                assert cols == width
+                assert rows == height
 
+                # get image dimensions for resizing
+                (resized_width, resized_height) = get_new_img_size(width, height, C.im_size)
 
+                # resize the image so that smalles side is length = 600px
+                x_img = cv2.resize(x_img, (resized_width, resized_height), interpolation=cv2.INTER_CUBIC)
 
+                try:
+                    y_rpn_cls, y_rpn_regr = calc_rpn(C, img_data_aug, width, height, resized_width, resized_height,
+                                                     img_length_calc_function)
+                except:
+                    print('Warning: fail to calc_rpn(in line: 348 from data_generators.py)')
+                    continue
 
+                # Zero-center by mean pixel, and preprocess image
 
+                x_img = x_img[:, :, (2, 1, 0)]  # BGR -> RGB [H, W, C]
+                x_img = x_img.astype(np.float32)
+                x_img[:, :, 0] -= C.img_channel_mean[0]
+                x_img[:, :, 1] -= C.img_channel_mean[1]
+                x_img[:, :, 2] -= C.img_channel_mean[2]
+                x_img /= C.img_scaling_factor
 
+                x_img = np.expand_dims(x_img, axis=0)  # [1, H, W, C]
 
+                y_rpn_regr[:, y_rpn_regr.shape[1] // 2:, :, :] *= C.std_scaling
 
+                y_rpn_cls = np.transpose(y_rpn_cls, (0, 2, 3, 1))
+                y_rpn_regr = np.transpose(y_rpn_regr, (0, 2, 3, 1))
 
+                yield np.copy(x_img), [np.copy(y_rpn_cls), np.copy(y_rpn_regr)], img_data_aug
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            except Exception as e:
+                print(e)
+                continue
